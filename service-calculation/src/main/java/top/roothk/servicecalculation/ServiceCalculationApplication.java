a4135055d1bba66f6364b7b263e34ce138b6f93c
roothk <hk374790498@gmail.com>
2018-05-03 13:13:23 +0800

    no message

--- service-calculation/src/main/java/top/roothk/servicecalculation/ServiceCalculationApplication.java
+++ service-calculation/src/main/java/top/roothk/servicecalculation/ServiceCalculationApplication.java
@@ -5,10 +5,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-@RefreshScope
 @EnableEurekaClient
 @EnableAutoConfiguration
 @SpringBootApplication
